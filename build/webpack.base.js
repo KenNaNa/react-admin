b28436a38d867473345eb79331bb8d30525e4780
KenNaNa <m13425477079@163.com>
2023-09-29 01:15:31 +0800

    处理字体和媒体文件

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -42,6 +42,31 @@ module.exports = {
           filename: 'static/images/[name][ext]', // 文件输出目录和命名
         },
       },
+
+      {
+        test: /.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
+        type: 'asset', // type选择asset
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
+          },
+        },
+        generator: {
+          filename: 'static/fonts/[name][ext]', // 文件输出目录和命名
+        },
+      },
+      {
+        test: /.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
+        type: 'asset', // type选择asset
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
+          },
+        },
+        generator: {
+          filename: 'static/media/[name][ext]', // 文件输出目录和命名
+        },
+      },
     ],
   },
 
