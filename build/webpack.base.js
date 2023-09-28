c4fc186a0eb2c5cbc45a7506e063f2f7ed9e53e0
KenNaNa <m13425477079@163.com>
2023-09-29 00:35:22 +0800

    配置解析 ts，tsx 插件

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -11,4 +11,23 @@ module.exports = {
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
     publicPath: '/', // 打包后文件的公共前缀路径
   },
+
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts,tsx文件
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // 预设执行顺序由左到右，所以先处理 ts，再处理tsx
+            presets: ['@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
 };
