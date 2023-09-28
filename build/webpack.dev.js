a05397032bafe3836de1412239eecabd0a235123
KenNaNa <m13425477079@163.com>
2023-09-29 00:45:31 +0800

    配置完成 webpack.dev，webpack.prod

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,1 +1,18 @@
 // webpack 开发环境配置
+const path = require('path');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+module.exports = merge(baseConfig, {
+  mode: 'development', // 开发模式,打包更加快速,省了代码优化步骤
+  devtool: 'eval-cheap-module-source-map', // 源码调试模式,后面会讲
+  devServer: {
+    port: 3000, // 服务端口号
+    compress: false, // gzip压缩,开发环境不开启,提升热更新速度
+    hot: true, // 开启热更新，后面会讲react模块热替换具体配置
+    historyApiFallback: true, // 解决history路由404问题
+    static: {
+      directory: path.join(__dirname, '../public'), //托管静态资源public文件夹
+    },
+  },
+});
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,1 +1,6 @@
 // 生产环境配置文件
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+module.exports = merge(baseConfig, {
+  mode: 'production', // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
+});
