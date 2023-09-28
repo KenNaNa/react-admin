d67d7f96fb1b863a83d9db9fddd990b09c6c6be8
KenNaNa <m13425477079@163.com>
2023-09-29 01:11:10 +0800

    配置复制 public

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,21 @@
 // 生产环境配置文件
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = merge(baseConfig, {
   mode: 'production', // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
+  plugins: [
+    // 复制文件插件
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'), // 复制public下文件
+          to: path.resolve(__dirname, '../dist'), // 复制到dist目录中
+          filter: (source) => {
+            return !source.includes('index.html'); // 忽略index.html
+          },
+        },
+      ],
+    }),
+  ],
 });
