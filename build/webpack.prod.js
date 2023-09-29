6ca137dc29d1366300ff698eb69c1efdc76e66f5
KenNaNa <m13425477079@163.com>
2023-09-29 09:25:28 +0800

    压缩js文件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,6 +4,7 @@ const baseConfig = require('./webpack.base.js');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = merge(baseConfig, {
   mode: 'production', // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
   // devtool: '', // 不用配置devtool此项
@@ -30,6 +31,16 @@ module.exports = merge(baseConfig, {
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(), // 压缩css
+
+      new TerserPlugin({
+        // 压缩js
+        parallel: true, // 开启多线程压缩
+        terserOptions: {
+          compress: {
+            pure_funcs: ['console.log'], // 删除console.log
+          },
+        },
+      }),
     ],
   },
 });
