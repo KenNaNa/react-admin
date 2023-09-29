878cafcd1e58af7b0a60f994c8c373772ec45b1d
KenNaNa <m13425477079@163.com>
2023-09-29 16:56:17 +0800

    3 files modified

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import { Image } from 'antd';
+import logoSrc from '../../assets/imgs/logo.png';
+
+const Logo: React.FC = () => {
+  return <Image width={200} src={logoSrc} />;
+};
+
+export default Logo;
