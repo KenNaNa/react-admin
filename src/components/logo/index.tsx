abdb7bb8684f90b0e7d6e622080bb62cf6ac6433
KenNaNa <m13425477079@163.com>
2023-09-29 16:59:43 +0800

    1 file modified

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image } from 'antd';
-import logoSrc from '../../assets/imgs/logo.png';
+const logoSrc =
+  'https://stackblitz.com/files/stackblitz-starters-fcc2gf/github/KenNaNa/react-admin/admin/src/assets/logo.png';
 
 const Logo: React.FC = () => {
   return <Image width={200} src={logoSrc} />;
