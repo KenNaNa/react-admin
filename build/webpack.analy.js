a51046682f2240f95f861d6e1ded657553e233a5
KenNaNa <m13425477079@163.com>
2023-09-29 09:19:07 +0800

    webpack包分析工具

--- build/webpack.analy.js
+++ build/webpack.analy.js
@@ -2,6 +2,13 @@ const prodConfig = require('./webpack.prod.js'); // 引入打包配置
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin'); // 引入webpack打包速度分析插件
 const smp = new SpeedMeasurePlugin(); // 实例化分析插件
 const { merge } = require('webpack-merge'); // 引入合并webpack配置方法
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // 引入分析打包结果插件
 
 // 使用smp.wrap方法,把生产环境配置传进去,由于后面可能会加分析配置,所以先留出合并空位
-module.exports = smp.wrap(merge(prodConfig, {}));
+module.exports = smp.wrap(
+  merge(prodConfig, {
+    plugins: [
+      new BundleAnalyzerPlugin(), // 配置分析打包结果插件
+    ],
+  })
+);
