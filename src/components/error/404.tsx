89752b2b0386ef4069e83b50e8f8d763e639f16c
KenNaNa <m13425477079@163.com>
2023-10-01 16:06:45 +0800

    处理动态路由菜单

---
+++ src/components/error/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Result } from 'antd';
+
+const Error404 = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="对不起，您访问的页面不存在！"
+    extra={<Button type="primary">返回首页</Button>}
+  />
+);
+
+export default Error404;
