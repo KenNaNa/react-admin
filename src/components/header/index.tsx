61a07a475e04eccc964549c02cfa115f1cfd6291
kennana <m13425477079@163.com>
2023-10-03 06:55:46 +0000

    封装退出功能

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Layout, Button } from 'antd';
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Layout, Button, Popconfirm } from 'antd';
+import { MenuFoldOutlined, MenuUnfoldOutlined, LogoutOutlined } from '@ant-design/icons';
 const { Header } = Layout;
+import './index.less'
+import useStore from '@/store';
+import { getItem, removeItem } from '@/utils/storage';
 
 export default function TopHeader({ colorBgContainer, collapsed, setCollapsed }: any) {
+    const { loginStore } = useStore()
+    const username = loginStore.username || getItem('username')
+    const onConfirm = () => {
+        window.location.href = '/login'
+        loginStore.isLogin = false
+        loginStore.token = ''
+        loginStore.username = ''
+
+        removeItem('isLogin')
+        removeItem('token')
+        removeItem('username')
+    }
     return (
-        <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Header style={{ padding: 0, background: colorBgContainer, display: 'flex', justifyContent: 'space-between' }}>
             <Button
                 type="text"
                 icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
@@ -16,6 +31,17 @@ export default function TopHeader({ colorBgContainer, collapsed, setCollapsed }:
                     height: 64,
                 }}
             />
+
+            <div className="user-info">
+                <span className="user-name">{ username }</span>
+                <span className="user-logout">
+                    <Popconfirm
+                        onConfirm={onConfirm}
+                        title="是否确认退出？" okText="退出" cancelText="取消">
+                        <LogoutOutlined /> 退出
+                    </Popconfirm>
+                </span>
+            </div>
         </Header>
     )
 }
