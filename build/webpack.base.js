80a30a7d6e78b4b93624990834f6ebbbd8beea8e
KenNaNa <m13425477079@163.com>
2023-10-05 14:55:47 +0800

    调整打包路径

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -3,7 +3,6 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const isDev = process.env.NODE_ENV === "development"; // 是否是开发模式
 module.exports = {
   // 打包入口
   entry: path.join(__dirname, "../src/index.tsx"),
@@ -13,7 +12,7 @@ module.exports = {
     filename: "static/js/[name].js", // 每个输出js的名称
     path: path.join(__dirname, "../dist"), // 打包结果输出路径
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
-    publicPath: isDev ? "/" : ".", // 打包后文件的公共前缀路径
+    publicPath: "/", // 打包后文件的公共前缀路径
   },
 
   resolve: {
